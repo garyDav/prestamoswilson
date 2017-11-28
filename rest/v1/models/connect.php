e45e9bc2f56532322d7806d14225c5458634a52c
Gary David Guzmán Muñoz <gary.2810.dav@gmail.com>
2017-11-27 22:35:40 -0400

    contraseña BD mod

--- rest/v1/models/connect.php
+++ rest/v1/models/connect.php
@@ -3,7 +3,8 @@
 function getConex() {
 	try {
 		$us = "root";
-		$pwd = "";
+		//$pwd = "";
+		$pwd = "garydavid";
 		$conex = new PDO("mysql:host=localhost;dbname=prestamos;charset=utf8",$us,$pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 		$conex->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	}catch(PDOException $e) {
